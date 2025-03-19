8708e4b5604286acf9bede54c7492f215e380616
Goldor20 <domitukor@gmail.com>
2025-03-19 10:34:44 +0100

    2-esével lép ez a rák

--- alap.py
+++ alap.py
@@ -7,24 +7,32 @@ varosfejl = open("varosfejlesztes.csv","r",encoding="utf8")
 ###############################
 temp = epuletek.readline()
 temp1 = temp.split(";")
+doublelist = []
 #print (temp.split(";"))
+# counter = 0
+# for i in epuletek:
+#     counter = counter + 1
+#     print (counter)
+########################
 for i in epuletek:
     temp = epuletek.readline()
     temp1 = temp.split(";")
     for i in temp1:
         print ("i= ",i)
+        doublelist.append(i)
+print ("list= ",doublelist)
 ##############################
-temp = lakosok.readline()
-temp1 = temp.split(";")
-print (temp.split(";"))
-##############################
-temp = services.readline()
-temp1 = temp.split(";")
-print (temp.split(";"))
-##############################
-temp = varosfejl.readline()
-temp1 = temp.split(";")
-print (temp.split(";"))
+# temp = lakosok.readline()
+# temp1 = temp.split(";")
+# print (temp.split(";"))
+# ##############################
+# temp = services.readline()
+# temp1 = temp.split(";")
+# print (temp.split(";"))
+# ##############################
+# temp = varosfejl.readline()
+# temp1 = temp.split(";")
+# print (temp.split(";"))
 ############################################################################################################
 
 #egyenlőre mindent int-re írtam be, de lehet némelyiket tömbbé/listává kell alakítani
